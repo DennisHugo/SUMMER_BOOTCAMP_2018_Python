4b6c4ced55a14fd5381f325a197667ac87a99fc7
dennishugo <dhtalamartinez@gmail.com>
2018-02-01 16:20:02 -0400

    L2T4 Subiendo el Cast

---
+++ lesson2/task4/type_cast.py
@@ -0,0 +1,9 @@
+
+
+
+number = 9
+print(type(number)) #imprime el tipo de variable "number"
+
+float_number = 9.0
+print(float_number)
+print(int(float_number)) #Convirtiendo a entero el punto flotante
