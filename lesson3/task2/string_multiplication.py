c8b9360032819f41792693bfc75a7b7442cbf921
dennishugo <dhtalamartinez@gmail.com>
2018-02-02 16:15:12 -0400

    DHTM Probando Python L3

---
+++ lesson3/task2/string_multiplication.py
@@ -0,0 +1,7 @@
+
+"""
+Python admite una multiplicación de cadena por número (¡pero no al revés!).
+"""
+hello = "hello "
+ten_of_hellos = hello * 10
+print(ten_of_hellos)
---
+++ lesson3/task6/in_operator.py
@@ -0,0 +1,10 @@
+
+"""
+Si desea verificar si una cadena contiene una letra específica o una subcadena
+puede usar la palabra reservada in.
+"""
+helado = "ice cream"
+print("cream" in helado)
+
+contains = "ca" in helado
+print(contains)
